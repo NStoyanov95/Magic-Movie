6ec26e7fa8f441ff77b7f3015aa399071e2b3fb4
NStoyanov95 <nstoyanov9512@gmail.com>
2024-01-31 19:41:32 +0200

    Hash password when register

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        lowercase: true,
     },
     password: {
         type: String,
         require: true,
     }
+})
+
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
 })
 
 const User = mongoose.model('User', userSchema);
