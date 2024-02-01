6d07bcda8807658b5869f4c57a986ec1aaf3ead8
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-01 19:54:06 +0200

    Create token with user data when login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,6 +17,14 @@ router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', async(req, res) => {
+    const {email, password} = req.body;
+
+   const token = await authService.login(email, password);
+
+    res.redirect('/');
+})
+
 
 
 module.exports = router;
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,3 +1,31 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+
+const { SECRET } = require('../utils/constants');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Invalid username or password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Invalid username or password');
+    }
+
+    const payload = {
+        _id: user._id,
+        email: user.email
+    };
+
+    const token = await jwt.sign(payload, SECRET);
+
+    return token;
+};
